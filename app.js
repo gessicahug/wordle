27950a9d948b2246f2f298d780111cf3a2b8f00a
gessica da silva <gessicahugendobler@gmail.com>
2022-07-04 16:56:46 -0300

    logica para a resposta correta

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const tileDisplay = document.querySelector('.tile-container')
 const keyboard = document.querySelector('.key-container')
 const messageDisplay = document.querySelector('.message-container')
 
+const wordle = 'SUPER'
+let gameOver = false
+
 const keys = [
   'Q',
   'W',
@@ -42,6 +45,9 @@ const guessRows = [
   ['', '', '', '', '']
 ]
 
+let currentRow = 0
+let currentTile = 0
+
 guessRows.forEach((guessRow, guessRowIndex) => {
   const rowElement = document.createElement('div')
   rowElement.setAttribute('id', 'guessRow-' + guessRowIndex)
@@ -54,14 +60,57 @@ guessRows.forEach((guessRow, guessRowIndex) => {
   tileDisplay.append(rowElement)
 })
 
-const handleClick = () => {
-  console.log('clicked')
-}
-
 keys.forEach(key => {
   const buttonElement = document.createElement('button')
   buttonElement.textContent = key
   buttonElement.setAttribute('id', key)
-  buttonElement.addEventListener('click', handleClick)
+  buttonElement.addEventListener('click', () => handleClick(key))
   keyboard.append(buttonElement)
 })
+
+const handleClick = (key) => {
+  if (key === '«') {
+    deleteLetter()
+    return
+  }
+  if (key === 'ENTER') {
+    checkRow()
+    return
+  }
+  addLetter(key)
+}
+
+const addLetter = (letter) => {
+  if (currentTile < 5 && currentRow < 6) {
+    const tile = document.getElementById('guessRow-' + currentRow + '-tile-' + currentTile)
+    tile.textContent = letter
+    guessRows[currentRow][currentTile] = letter
+    tile.setAttribute('data', letter)
+    currentTile++
+  }
+}
+const deleteLetter = () => {
+  if (currentTile > 0) {
+    currentTile--
+    const tile = document.getElementById('guessRow-' + currentRow + '-tile-' + currentTile)
+    tile.textContent = ''
+    guessRows[currentRow][currentTile] = ''
+    tile.setAttribute('data', '')
+  }
+}
+
+const checkRow = () => {
+  const guess = guessRows[currentRow].join('')
+  if (currentTile === 5) {
+    if (wordle == guess) {
+      showMessage('Magnificent!')
+    }
+  }
+}
+
+const showMessage = (message) => {
+  const messageElement = document.createElement('p')
+  messageElement.textContent = message
+  messageDisplay.append(messageElement)
+  setTimeout(() => messageDisplay.removeChild(messageElement), 2000)
+}
