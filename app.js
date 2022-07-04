461fa2854d0f8d015d2883125654f5386f597434
gessica da silva <gessicahugendobler@gmail.com>
2022-07-04 15:29:26 -0300

    keyboard estilizado

--- app.js
+++ app.js
@@ -1,3 +1,45 @@
 const tileDisplay = document.querySelector('.tile-container')
 const keyboard = document.querySelector('.key-container')
 const messageDisplay = document.querySelector('.message-container')
+
+const keys = [
+  'Q',
+  'W',
+  'E',
+  'R',
+  'T',
+  'Y',
+  'U',
+  'I',
+  'O',
+  'P',
+  'A',
+  'S',
+  'D',
+  'F',
+  'G',
+  'H',
+  'J',
+  'K',
+  'L',
+  'ENTER',
+  'Z',
+  'X',
+  'C',
+  'V',
+  'B',
+  'N',
+  'M',
+  '«',
+]
+const handleClick = () => {
+  console.log('clicked')
+}
+
+keys.forEach(key => {
+  const buttonElement = document.createElement('button')
+  buttonElement.textContent = key
+  buttonElement.setAttribute('id', key)
+  buttonElement.addEventListener('click', handleClick)
+  keyboard.append(buttonElement)
+})
