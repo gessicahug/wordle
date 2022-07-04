653164cdc71c2c80d4194b505575ff2b26c1c72f
gessica da silva <gessicahugendobler@gmail.com>
2022-07-04 15:43:55 -0300

    initial tiles

--- app.js
+++ app.js
@@ -32,6 +32,27 @@ const keys = [
   'M',
   '«',
 ]
+
+const guessRows = [
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', ''],
+  ['', '', '', '', '']
+]
+
+guessRows.forEach((guessRow, guessRowIndex) => {
+  const rowElement = document.createElement('div')
+  rowElement.setAttribute('id', 'guessRow-' + guessRowIndex)
+  guessRow.forEach((guess, guessIndex) => {
+    const tileElement = document.createElement('div')
+    tileElement.setAttribute('id', 'guessRow-' + guessRowIndex + '-tile-' + guessIndex)
+    rowElement.append(tileElement)
+  })
+  tileDisplay.append(rowElement)
+})
+
 const handleClick = () => {
   console.log('clicked')
 }
