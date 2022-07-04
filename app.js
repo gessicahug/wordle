72d38c18520dff7cb88ae843e36170dedcaf9fd1
gessica da silva <gessicahugendobler@gmail.com>
2022-07-04 18:26:32 -0300

    cor para o keyboard também

--- app.js
+++ app.js
@@ -127,17 +127,27 @@ const showMessage = (message) => {
   messageDisplay.append(messageElement)
   setTimeout(() => messageDisplay.removeChild(messageElement), 2000)
 }
+const addColorToKey = (keyLetter, color) => {
+  const key = document.getElementById(keyLetter)
+  key.classList.add(color)
+}
 
 const flipTile = () => {
   const rowTiles = document.querySelector('#guessRow-' + currentRow).childNodes
   rowTiles.forEach((tile, index) => {
     const dataLetter = tile.getAttribute('data')
-    if (dataLetter == wordle[index]) {
-      tile.classList.add('green-overlay')
-    } else if (wordle.includes(dataLetter)){
-      tile.classList.add('yellow-overlay')
-    } else {
-      tile.classList.add('grey-overlay')
-    }
+    setTimeout(() => {
+      tile.classList.add('flip')
+      if (dataLetter == wordle[index]) {
+        tile.classList.add('green-overlay')
+        addColorToKey(dataLetter, 'green-overlay')
+      } else if (wordle.includes(dataLetter)){
+        tile.classList.add('yellow-overlay')
+        addColorToKey(dataLetter, 'yellow-overlay')
+      } else {
+        tile.classList.add('grey-overlay')
+        addColorToKey(dataLetter, 'grey-overlay')
+      }
+    }, 400 * index)
   })
 }
