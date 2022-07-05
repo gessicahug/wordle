7167f4ad416d781be47c436c0a64de41e9fdd3e1
gessica da silva <gessicahugendobler@gmail.com>
2022-07-05 13:58:39 -0300

    api para checar se a palavra existe no dicionário

--- index.js
+++ index.js
@@ -11,39 +11,39 @@ app.get('/word', (req, res) => {
   const options = {
     method: 'GET',
     url: 'https://random-words5.p.rapidapi.com/getMultipleRandom',
-    params: {count: '5', wordLength: '5'},
+    params: { count: '5', wordLength: '5' },
     headers: {
-      'X-RapidAPI-Key': process.env.RAPID_API_KEY,
-      'X-RapidAPI-Host': 'random-words5.p.rapidapi.com'
+      'x-rapidapi-host': 'random-words5.p.rapidapi.com',
+      'x-rapidapi-key': process.env.RAPID_API_KEY
     }
-  };
-    axios.request(options).then((response) => {
-        console.log(response.data)
-        res.json(response.data[0])
-    }).catch((error) => {
-        console.error(error)
-    })
+  }
+  axios.request(options).then((response) => {
+    console.log(response.data)
+    res.json(response.data[0])
+  }).catch((error) => {
+    console.error(error)
+  })
 })
 
 
 app.get('/check', (req, res) => {
-    const word = req.query.word
+  const word = req.query.word
 
-    const options = {
-        method: 'GET',
-        url: 'https://twinword-word-graph-dictionary.p.rapidapi.com/association/',
-        params: {entry: word},
-        headers: {
-            'x-rapidapi-host': 'twinword-word-graph-dictionary.p.rapidapi.com',
-            'x-rapidapi-key': process.env.RAPID_API_KEY
-        }
+  const options = {
+    method: 'GET',
+    url: 'https://twinword-word-graph-dictionary.p.rapidapi.com/association/',
+    params: { entry: word },
+    headers: {
+      'x-rapidapi-host': 'twinword-word-graph-dictionary.p.rapidapi.com',
+      'x-rapidapi-key': process.env.RAPID_API_KEY
     }
-    axios.request(options).then((response) => {
-        console.log(response.data)
-        res.json(response.data.result_msg)
-    }).catch((error) => {
-        console.error(error)
-    })
+  }
+  axios.request(options).then((response) => {
+    console.log(response.data)
+    res.json(response.data.result_msg)
+  }).catch((error) => {
+    console.error(error)
+  })
 })
 
 
