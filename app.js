bd6d4d2f752143bef233f9bf24a32222303ae5ec
gessica da silva <gessicahugendobler@gmail.com>
2022-07-05 11:11:26 -0300

    att

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const tileDisplay = document.querySelector('.tile-container')
 const keyboard = document.querySelector('.key-container')
 const messageDisplay = document.querySelector('.message-container')
 
-const wordle = 'SUPER'
+const wordle = 'BARCO'
 
 const keys = [
   'Q',
